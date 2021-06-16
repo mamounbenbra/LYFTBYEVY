b26bfc22a9c7e9515e47cf3cefcd2007e53453d1
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-06-17 01:48:32 +0200

    oui

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
 TYPES = ["Type 1", "Type2"]
-TIMES = ["10H-11H40", "14H10-15H50", "16H20-18H", "18H-20H"]
+TIMES = ["10H-11H41", "14H10-15H50", "16H20-18H", "18H-20H"]
 LOCATIONS = ["Casablanca", "Paris"]
 belongs_to :user
 validates_uniqueness_of :time, scope: :date
