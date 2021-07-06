07bed0bd567503744ed36a19ad1c41527fad6872
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-07-06 21:44:05 +0100

    oui

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_232450) do
+ActiveRecord::Schema.define(version: 2021_07_05_193953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 2021_06_16_232450) do
     t.string "time"
     t.string "location"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "contact_forms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "locations", force: :cascade do |t|
