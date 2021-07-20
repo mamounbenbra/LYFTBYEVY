8c518ab2b1a064cb2b5ec15e90f0233bca8db295
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-07-21 00:31:42 +0100

    oui

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :contacts, only: [:new, :create]
+  get '/contacts', to: 'contacts#new', as:'contact'
   get '/create1', to: 'contacts#create1', as:'create1'
   get '/renata', to: 'pages#renata', as: 'renata'
   get '/lyft', to: 'pages#lyft', as: 'lyft'
