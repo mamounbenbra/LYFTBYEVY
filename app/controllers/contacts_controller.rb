271b5d43601526d5e323408eb9110d8fe537ecb3
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-07-20 21:01:25 +0100

    oui

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,11 @@
 class ContactsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:new, :create1, :create]
 
   def new
     @contact = Contact.new
+  end
+
+  def create1
   end
 
   def create
@@ -10,6 +13,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
+      redirect_to create1_path
     else
       flash.now[:error] = 'Message impossible à envoyer'
       render :new
