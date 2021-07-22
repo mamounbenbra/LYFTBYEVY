1a0dac77adf4f6adfbdcfdabd16c803594947fa6
Mamoun Benbrahim <mamounbenbra@gmail.com>
2021-07-22 01:12:20 +0100

    oui

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :renata ]
+  skip_before_action :authenticate_user!, only: [:home, :renata, :lyft]
 
   def home
   end
