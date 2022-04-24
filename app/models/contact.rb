78caf0c2d0d8d11277999763d52edf828b5bb0b6
Mamoun Benbrahim <mamounbenbra@gmail.com>
2022-04-24 11:55:44 +0000

    oui

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   def headers
     {
       subject: "Message sur le site LYFT BY EVY",
-      to: "evy@tomcito.net",
+      to: "nancy.tomcito@me.com",
       from: %("#{name}" <#{email}>)
     }
   end
